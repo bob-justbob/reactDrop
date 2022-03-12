f2d9bdf88ad3973280206999ce04e3101361db7b
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2022-03-12 17:26:18 +0400

    added E section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import Asection from './sectionA/Asection';
 import Bsection from './sectionB/Bsection';
 import Csection from './sectionC/Csection';
 import Dsection from './sectionD/Dsection';
+import Esection from './sectionE/Esection';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <iframe width="560" height="315" src="https://www.youtube.com/embed/ig3CLaRdjk4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
       <Dsection/>
+      <Esection />
     </div>
   );
 }
---
+++ my-app/src/sectionE/Esection.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Esection.css'
+
+
+export default function() {
+    return (
+        <div className="Esection">
+            <div className="E-top">
+                <ul>
+                    <li>WHAT IS DROPSHIPPING</li>
+                    <li>FEATURES</li>
+                    <li>ABOUT</li>
+                    <li>WHY 365DROPSHIP</li>
+                </ul>
+            </div>
+            <div className="E-bot">
+                <div>
+                    <h2>What is 365DROPSHIP?</h2>
+                    <p>365dropship allows you to add high demand dropship products from our extensive catalog to sell at your store, ship the orders directly to customers all over the world, and help increase your profits an excellent solution for people with existing stores anywhere online and those of you looking to break into the eCommerce market. We propose you to download dropshipping products from the hundreds of best-selling products from a variety of niches with affordable prices to choose from our catalog. We have teamed up with some of the best worldwide drop ship suppliers in order to create an attractive line of products, thousands of </p>
+                    <h2>What is dropshipping?</h2>
+                    <p>Selling high demand products without actually stocking or shipping. Never worrying about an item not selling. You only pay for what you sell. Access to the hottest name brand products from our huge inventory catalog to sell on your store. Risk-free online business creation and management. Focusing on the important stuff that you do best: Generating traffic and sales.</p>
+                    <p>Our dropshipping service is ideal for anyone, no matter what dropship items your customers may choose they’ll be getting quality and name-brand pieces. You can choose products from basic, everyday apparel to luxurious selections. 365 Dropship platform makes it easy to automate dropshipping products to your eCommerce store. </p>
+                    <p>Tap into our product catalog and choose from a large variety of products to build your business inventory. With our highly advanced Dropship integration tool, you can sync our dropshipping product data feed and eCommerce tools directly with your current website.</p>
+                    <p> You can sell our products on the most popular platforms. With our easy to use automatic product feed tools. if you wanted to sell our products on Shopify, WooCommerce or Magento, then you would simply choose the items you want to sell and export them to your site. All our feeds are per-formatted to meet the requirements of several marketplace websites so you don’t have to worry about maintaining the feeds yourself.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
